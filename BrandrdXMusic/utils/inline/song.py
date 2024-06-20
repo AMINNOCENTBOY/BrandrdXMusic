80e43b403480ea0363a5cab2b05688dee4e40085
AMINNOCENTT <155085235+AMINNOCENTT@users.noreply.github.com>
2024-06-20 11:23:37 +0530

    Update song.py

--- BrandrdXMusic/utils/inline/song.py
+++ BrandrdXMusic/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_CHAT}",
+                text="✭𝙶𝚁𝙾𝚄𝙿✭", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
